a1da191bdbc0ea060711b5be2e0ccc4d85f044c5
Pedro vaz <vazdovalep@gmail.com>
2023-08-30 09:20:08 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Turista mochileiro = new Turista();
+        // Definir formato do objeto
+        mochileiro.nome = "Lindsay Loham";
+        mochileiro.cpf = '123';
+        Turista mochileiro = new Turista();
+        mochileiro.nome = "James Gun";
+        // Definir comportamento
+     String retorno   mochileiro.viajar();
+    }
+}
---
+++ src/Turista.java
@@ -0,0 +1,15 @@
+public class Turista {
+    String nome;
+    String cpf;
+
+    public String viajar(){
+        return "Viajando...";
+    }
+    // Construtor
+    Turista(String _name){
+        this.name = _name;
+    }
+
+    // Encapsulamento - Getters 
+
+    }
